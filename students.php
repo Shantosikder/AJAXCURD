4bce836b14687207aec57c0351906743ddc4a20b
shantosikder <shantosikder77@gmailcom>
2022-03-03 22:29:31 +0600

    first

---
+++ students.php
@@ -0,0 +1,14 @@
+<?php
+
+ $id = $_GET['id'];
+
+ $db = new mysqli("localhost", "root", "", "ajax");
+ 
+ $sql = "SELECT * FROM student where round_id ='$id'";
+   $data = $db->query($sql);
+
+ while($row =$data->fetch_object()){
+    echo "<tr><td>".$row->name."</td><td>".$row->email."</td><td>".$row->gender."</td></tr>";
+ }
+
+?>
